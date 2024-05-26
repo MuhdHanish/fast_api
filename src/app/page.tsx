dee5d10bce7ba09409a540daf5e2227be12b6e87
MuhdHanish <muhammedhanish11@gmail.com>
2024-05-26 14:09:21 +0530

    added basic css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   const [searchResults, setSearchResults] = useState<TSearchResult>({ results: [], duration: 0 });
   useEffect(() => {
     const fetchData = async () => {
-      if(!input) return setSearchResults({ results: [], duration: 0 });
+      if (!input) return setSearchResults({ results: [], duration: 0 });
       try {
         const response = await fetch(`/api/search?query=${input}`);
         if (!response.ok) {
@@ -23,15 +23,17 @@ export default function Home() {
     }
     fetchData();
   }, [input]);
-  
+
   return (
-    <div>
-      <input
-        type="text" 
-        value={input}
-        className="text-zinc-900"
-        onChange={(event) => setInput(event.target.value)}
+    <main className="h-screen w-screen grainy">
+      <div className="flex flex-col gap-6 items-center pt-32 duration-500 animate-in animate fade-in-5 slide-in-from-bottom-2.5">
+        <input
+          type="text"
+          value={input}
+          className="text-zinc-900"
+          onChange={(event) => setInput(event.target.value)}
         />
-    </div>
+      </div>
+    </main>
   );
 }
