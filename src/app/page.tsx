e63836cebb9926a7dffc3c49b03d26601fa68014
MuhdHanish <muhammedhanish11@gmail.com>
2024-05-26 16:53:18 +0530

    showed the results of search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,29 @@
 "use client";
 
 import { TSearchResult } from "@/types";
+import { fetchCountries } from "@/utils";
 import { useEffect, useState } from "react";
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "@/components/ui/command";
 
 export default function Home() {
   const [input, setInput] = useState("");
-  const [searchResults, setSearchResults] = useState<TSearchResult>({ results: [], duration: 0 });
+  const [error, setError] = useState("");
+  const handleInputChange = (value: string) => setInput(value.trim());
+  const [searchResults, setSearchResults] = useState<TSearchResult>({
+    results: [],
+    duration: 0,
+  });
   useEffect(() => {
     const fetchData = async () => {
       if (!input) return setSearchResults({ results: [], duration: 0 });
+      setError("");
       try {
-        const response = await fetch(`/api/search?query=${input}`);
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const data: TSearchResult = await response.json();
+        const data: TSearchResult = await fetchCountries(input, setError);
         setSearchResults(data);
       } catch (error) {
-        console.error('Error fetching data:', error);
         setSearchResults({ results: [], duration: 0 });
       }
-    }
+    };
     fetchData();
   }, [input]);
 
@@ -30,17 +33,39 @@ export default function Home() {
         <h1 className="text-5xl tracking-tight font-bold">Speed Search</h1>
         <p className="text-zinc-600 text-lg max-w-prose text-center">
           A Hight-Performance API Built With Hono, Next.JS & Cloudflare. <br />
-          <span className="text-base">Type A Qurey Below & Get Your Results In Miliseconds</span>
+          <span className="text-base">
+            Type A Qurey Below & Get Your Results In Miliseconds
+          </span>
         </p>
         <div className="max-w-md w-full">
-          
+          <Command>
+            <CommandInput
+              value={input}
+              onValueChange={handleInputChange}
+              placeholder="Search countries..."
+              className="placeholder:text-zinc-500"
+            />
+            <CommandList>
+              {
+                !searchResults?.results.length ? input && !error && <CommandEmpty>No Results Found !</CommandEmpty>
+                  : input && <CommandGroup heading="Results">
+                    {
+                      searchResults?.results.map((result) => (
+                        <CommandItem
+                          key={result}
+                          value={result}
+                          onSelect={setInput}
+                        >
+                          {result}
+                        </CommandItem>
+                      ))
+                    }
+                  </CommandGroup>
+              }
+              {error && input && <CommandEmpty><span className="text-red-600">{error}</span></CommandEmpty>}
+            </CommandList>
+          </Command>
         </div>
-        <input
-          type="text"
-          value={input}
-          className="text-zinc-900"
-          onChange={(event) => setInput(event.target.value)}
-        />
       </div>
     </main>
   );
