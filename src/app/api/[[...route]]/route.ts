6bc63df000bc9fe9f1751bab2826ac512fecc1a1
MuhdHanish <muhammedhanish11@gmail.com>
2024-05-26 13:31:32 +0530

    completed the handler of /api/search

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,12 +1,44 @@
 import { Hono } from "hono";
+import { env } from "hono/adapter";
 import { handle } from "hono/vercel";
+import { TEnvConfig } from "@/types";
+import { Redis } from "@upstash/redis";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-app.get("/search", (context) => {
-    return context.json({});
+app.get("/search", async (context) => {
+    try {
+        const query = context.req.query("query")?.toUpperCase();
+        if (!query) return context.json({ message: "Invalid Search Query" }, 400);
+        
+        const { UPSTASH_REDIS_REST_URL, UPSTASH_REDIS_REST_TOKEN } = env<TEnvConfig>(context);
+        const redis = new Redis({
+            url: UPSTASH_REDIS_REST_URL,
+            token: UPSTASH_REDIS_REST_TOKEN
+        });
+
+        const startTime = performance.now();
+        
+        const results: string[] = [];
+        const rank = await redis.zrank("terms", query);
+        if (rank !== null && rank !== undefined) {
+            const temp = await redis.zrange<string[]>("terms", rank, rank + 100);
+            for (const element of temp) {
+                if (!element.startsWith(query)) break;
+                if (element.endsWith("*")) results.push(element.substring(0, element.length - 1));
+            }
+        }
+
+        const endTime = performance.now();
+        const duration = endTime - startTime;
+
+        return context.json({ results, duration }, 200);
+    } catch (error) {
+        console.error('Error during search operation:', error);
+        return context.json({ message: "Internal Server Error", error: error instanceof Error ? error.message : "Unexpected Error" }, 500);
+    }
 });
 
 export const GET = handle(app);
