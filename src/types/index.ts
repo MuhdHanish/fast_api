11047effa020fee9d0b1fd7c9489c3462088307b
MuhdHanish <muhammedhanish11@gmail.com>
2024-05-26 12:49:36 +0530

    added new type for env config

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,9 @@
 export type TSearchResult = {
     results: string[];
     duration: number;
-}
+};
+
+export type TEnvConfig = {
+    UPSTASH_REDIS_REST_URL: string;
+    UPSTASH_REDIS_REST_TOKEN: string;
+};
